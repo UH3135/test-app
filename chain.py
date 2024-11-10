ff3b689cf9575972748a1799498d10e1963230ed
UH3135 <rabbitdy0101@kookmin.ac.kr>
2024-11-10 20:34:51 +0900

    Update chain.py

--- chain.py
+++ chain.py
@@ -8,7 +8,7 @@ load_dotenv()
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful, smart, kind, and efficient AI assistant. You always fulfill the user's requests to the best of your ability. You always answer succinctly. You must answer in Korean."),
     ("user", "{user_input}"),
-    MessagesPlaceholder(variable_name="messages1")
+    MessagesPlaceholder(variable_name="messages")
 ])
 
 llm = ChatUpstage()
